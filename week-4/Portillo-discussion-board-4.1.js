9cf977f72ee876c6f753833d7e9d0d3f0585f2e3
dearwendy <dearwendy714@gmail.com>
2019-06-19 16:56:27 -0700

    Commit
    Commit Discussion Board 3.1

---
+++ week-4/Portillo-discussion-board-4.1.js
@@ -0,0 +1,55 @@
+/*
+ Title: Discussion Board 3.1
+ Author: Tania Rascia (www.digitalocean.com)
+ Date: 17 June 2019
+ Modified By: Wendy Portillo
+ Description: Practice simple arrays by creating an array literal,
+ using indices to access array members and use methods like push,
+ join, pop and shift to manipulate the array. I added three errors that will
+ need to be corrected before the program will run.
+ */
+
+
+// Load additional JavaScript file
+var header = require("../header.js");
+
+// Output Header Display
+console.log(header.display("Wendy", "Portillo", "Discussion Board 4.1") + "\n");
+
+// Start program
+
+
+// Create a new array named languages that contains at least 7 programming languages.
+let languages = ['JavaScript','Python', 'Java', 'C#', 'PHP', 'Swift', 'Ruby'];
+
+// console.log to display the number of elements in the array:
+console.log(languageslength);
+
+/* Use console.log to list the first element from the array using an index value. */
+console.log(languages[0]);
+
+/* Use console.log to list the last element from the array using an index value. */
+console.log(languages[languages.length 7]);
+
+
+/*
+ Title: Discussion Board 3.1
+ Author: JavaScript for Cats (http://jsforcats.com/#arrays)
+ Date: 17 June 2019
+ Modified By: Wendy Portillo
+ Description: Practice simple arrays by creating an array literal,
+ using indices to access array members and use methods like push,
+ join, pop and shift to manipulate the array. I added three errors that will
+ need to be corrected before the program will run.
+ */
+
+
+let myCatFriends = ["bill","tabby", "ceiling"]
+
+console.log(myCatFriends.push("super hip cat"))
+console.log(myCatFriends.length);
+console.log(myCatFriends (0);
+console.log(myCatFriends [4]);
+
+
+
