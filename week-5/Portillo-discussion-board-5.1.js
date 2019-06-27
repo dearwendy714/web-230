2cd3869db9504eab33ddd989c47dd6abb5b1fba0
dearwendy <dearwendy714@gmail.com>
2019-06-27 11:57:21 -0700

    Complete Assignment

---
+++ week-5/Portillo-discussion-board-5.1.js
@@ -0,0 +1,40 @@
+/*
+============================================
+ Title:  Discussion Board-5.1.js
+ Author: Mozilla (developer.mozilla.org)
+ Modified By: Wendy Portillo
+ Date:   June 27, 2019
+ Description: Simple program on how to use
+       arrays and operators.
+===========================================
+*/
+// Load additional JavaScript file
+var header = require("../header.js");
+
+
+// Outputs the header to the console
+console.log(header.display("Wendy", "Portillo", "Exercise 5.1") + '\n');
+
+// Start program
+
+// Create a map
+
+var sayings = new Map()
+sayings.set('dog', 'woof');
+sayings.set('cat, 'meow');
+sayings.set('elephant', 'toot');
+sayings.size; // 3
+sayings.get('fox'); // undefined
+sayings.has('bird'); // false
+sayings.dele('dog');
+sayings.has('dog'); // false
+
+fr (var [key, value] of sayings) {
+  console.log(key + ' goes ' + value);
+}
+// "cat goes meow"
+// "elephant goes toot"
+
+sayings.clear();
+sayings.size; // 0
+
