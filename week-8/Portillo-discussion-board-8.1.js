ccaa3293be9211ae263a714452313411eabd605d
dearwendy <dearwendy714@gmail.com>
2019-07-19 14:41:51 -0700

    Complete assignment

---
+++ week-8/Portillo-discussion-board-8.1.js
@@ -0,0 +1,18 @@
+/*
+============================================
+ Title:  Discussion Board-8.1.js
+ Author: Mozilla (developer.mozilla.org)
+ Modified By: Wendy Portillo
+ Date:   June 27, 2019
+ Description: Simple program on how to use
+       arrays and operators.
+===========================================
+*/
+// Load additional JavaScript file
+var header = require("../header.js");
+
+
+// Outputs the header to the console
+console.log(header.display("Wendy", "Portillo", "Exercise 8.1") + '\n');
+
+// Start program
